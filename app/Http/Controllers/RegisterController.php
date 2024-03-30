2cdd4c8d102e7b2f9ce6e24e0a7186d5cbcb141b
bramaditya <bramadityaw@duck.com>
2024-03-30 21:35:13 +0700

    make registration controller

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\View\View;
+
+class RegisterController extends Controller
+{
+    public function index() : View
+    {
+        return view('auth.register');
+    }
+
+    public function register(Request $request) : RedirectResponse
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', ''],
+        ]);
+
+        User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password']),
+        ]);
+
+        return redirect()->intended('toko');
+    }
+
+}
