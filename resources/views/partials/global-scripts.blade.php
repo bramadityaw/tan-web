0ea69448aa3fe837532958edb92b241f819027f6
bramaditya <bramadityaw@duck.com>
2024-04-03 11:04:52 +0700

    account options togglable

---
+++ resources/views/partials/global-scripts.blade.php
@@ -0,0 +1,16 @@
+<script>
+const profilePic = document.querySelector('nav #profile');
+const profileOpts = document.querySelector('nav + div');
+
+
+function toggleProfileOpts() {
+    if (profileOpts.classList.contains("hidden")){
+        profileOpts.classList.remove("hidden");
+    }
+    else {
+        profileOpts.classList.add("hidden")
+    }
+}
+
+profilePic?.addEventListener("click", toggleProfileOpts);
+</script>
