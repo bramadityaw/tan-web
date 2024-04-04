bfb212496540c103f3393eec697a839eaa73840e
bramaditya <bramadityaw@duck.com>
2024-04-04 17:37:28 +0700

    create transactions table

---
+++ database/migrations/2024_04_04_095101_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('verified_at');
+            $table->enum('verif_status', ['pending', 'verified', 'failed']);
+            $table->string('verif_bukti'); //url ke gambar screenshoot bukti transaksi
+            $table->unsignedInteger('total_bayar');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
