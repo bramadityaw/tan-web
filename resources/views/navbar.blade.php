38b93f34abaa5496ec6c865e72a8533588be6e17
bramaditya <bramadityaw@duck.com>
2024-04-17 08:45:22 +0700

    slim out navbar padding and add m-x to search bar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,9 +1,9 @@
 <header class="bg-[#1b3c73] sticky top-0 z-10">
-    <nav class=" flex px-8 py-5 text-white items-center justify-between">
+    <nav class=" flex px-6 py-4 text-white items-center justify-between">
         @include('partials.icons.logo')
-        <div class="rounded-md bg-white text-[#909090] w-full sm:w-[60%] lg:w-[35%]">
+        <div class="rounded-md bg-white text-[#909090] mx-2 w-full sm:w-[60%] lg:w-[35%]">
             <form action="" class="flex m-0">
-                <input class="text-base w-full border-0 rounded-l-md pl-4 py-3" type="text" placeholder="Cari ikan hias, pakan, aksesoris akuarium...">
+                <input class="text-base w-full border-0 rounded-l-md pl-2 py-1" type="text" placeholder="Cari ikan hias, pakan, aksesoris akuarium...">
                 <button class="px-4" type="submit">
                     <i class="fa fa-search"></i>
                 </button>
