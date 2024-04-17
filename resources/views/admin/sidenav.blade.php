a92cd082d3b65a30090d1598f83fd2cbb95d2e9c
bramaditya <bramadityaw@duck.com>
2024-04-17 20:00:38 +0700

    add sidenav to admin layout

---
+++ resources/views/admin/sidenav.blade.php
@@ -0,0 +1,12 @@
+<aside class="w-1/5 bg-gray-800">
+    <nav>
+    <ul>
+        <li>
+            <a href="/admin/dashboard">Beranda</a>
+        </li>
+        <li>
+            <a href="/admin/dashboard/transaksi">Transaksi</a>
+        </li>
+    </ul>
+    </nav>
+</aside>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,4 +1,5 @@
 @include('head')
+@include('admin.sidenav')
 <main>
 
 @yield('main')
