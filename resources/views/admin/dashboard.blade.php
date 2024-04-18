87d9dcd3387eebba6598eec80c9d1a6124756dec
bramaditya <bramadityaw@duck.com>
2024-04-18 07:03:24 +0700

    add placeholder content for dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.admin')
 @section('main')
-    <h1>Admin Page</h1>
+    <section>
+        <div class="text-center">
+            <img src="/images/under-construction.jpeg" class="mx-auto aspect-auto w-2/5" alt="Masih dalam pengembangan">
+            <h1 class="text-2xl">Bagian ini masih dalam pengembangan.</h1>
+        </div>
+        <p class="text-center">Halaman ini nantinya akan berisi informasi mengenai rangkuman penjualan bulanan dan tahunan.</p>
+    </section>
 @endsection
