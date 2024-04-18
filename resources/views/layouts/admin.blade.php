a5b457ea604fa44c99d59893e87856c79d366d7c
bramaditya <bramadityaw@duck.com>
2024-04-18 07:10:17 +0700

    fixed returned url

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,17 +1,39 @@
 @php
 function admin($url = '') {
-    return '/admin/dashboard' . $url;
+    return '/admin/dashboard/' . $url;
 }
 @endphp
 
 @include('head')
     <div class="flex">
     @include('admin.sidenav')
-        <main>
+        <div class="w-full">
+            @include('admin.topnav')
+                <main class="px-6">
 
-        @yield('main')
+                @yield('main')
 
-        </main>
+                </main>
+        </div>
     </div>
+@section('scripts')
+    <script>
+        const profilePic = document.querySelector('nav #profile');
+        const profileOpts = document.querySelector('nav + div');
+
+
+        function toggleProfileOpts() {
+            if (profileOpts.classList.contains("hidden")){
+                profileOpts.classList.remove("hidden");
+            }
+            else {
+                profileOpts.classList.add("hidden")
+            }
+        }
+
+        profilePic?.addEventListener("click", toggleProfileOpts);
+    </script>
+@show
+
 </body>
 </html>
