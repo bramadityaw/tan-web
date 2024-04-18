c86cf8581d338aca3c313a056a37afe5a7981955
bramaditya <bramadityaw@duck.com>
2024-04-18 07:14:39 +0700

    complete menu urls

--- resources/views/admin/sidenav.blade.php
+++ resources/views/admin/sidenav.blade.php
@@ -1,12 +1,24 @@
-<aside class="w-1/5 bg-gray-800">
-    <nav>
-    <ul>
-        <li>
-            <a href="{{ admin() }}">Beranda</a>
-        </li>
-        <li>
-            <a href="{{ admin('transaksi') }}">Transaksi</a>
-        </li>
-    </ul>
-    </nav>
+<aside class="hidden md:block w-1/5 py-4 bg-gray-800 text-white">
+    <div class="w-4/5 h-[100lvh] mx-auto">
+    @include('partials.icons.logo')
+        <nav>
+            <ul>
+                <li class="mb-2">
+                    <a href="{{ admin() }}">Beranda</a>
+                </li>
+                <li class="mb-2">
+                    <a href="{{ admin('transactions') }}">Kelola Transaksi</a>
+                </li>
+                <li class="mb-2">
+                    <a href="{{ admin('products') }}">Kelola Produk</a>
+                </li>
+                <li class="mb-2">
+                    <a href="{{ admin('blog') }}">Kelola Blog</a>
+                </li>
+                 <li class="mb-2">
+                    <a href="{{ admin('users') }}">Kelola Akun</a>
+                </li>
+           </ul>
+        </nav>
+    </div>
 </aside>
