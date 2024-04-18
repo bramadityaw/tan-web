5e9d1a4f722d4a32e4d37b6b8c791df0d1da644b
bramaditya <bramadityaw@duck.com>
2024-04-19 05:40:01 +0700

    fix map embed overflow

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -105,8 +105,8 @@
                 </div>
 
            </div>
-           <div class="m-9 mr-0">
-               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.2374267719338!2d103.80543709999999!3d-3.758419!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e39f185bd2f6ab3%3A0xa5299171edf97a05!2sTan%20Aquatic!5e0!3m2!1sen!2sid!4v1710311797638!5m2!1sen!2sid" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+           <div class="mx-auto mt-9 sm:m-9 sm:mr-0">
+               <iframe class="block w-full rounded-l-lg" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.2374267719338!2d103.80543709999999!3d-3.758419!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e39f185bd2f6ab3%3A0xa5299171edf97a05!2sTan%20Aquatic!5e0!3m2!1sen!2sid!4v1710311797638!5m2!1sen!2sid" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
            </div>
 
     </div>
