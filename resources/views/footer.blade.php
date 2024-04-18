588e45e90cb6ae6879d6ef877dc4477f66143066
bramaditya <bramadityaw@duck.com>
2024-04-19 05:18:24 +0700

    fix email typo

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -40,7 +40,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" width="23" height="18" viewBox="0 0 23 18" fill="none">
                             <path d="M22.1786 0H0.821429C0.367076 0 0 0.365625 0 0.818182V17.1818C0 17.6344 0.367076 18 0.821429 18H22.1786C22.6329 18 23 17.6344 23 17.1818V0.818182C23 0.365625 22.6329 0 22.1786 0ZM21.1518 2.83295V16.1591H1.84821V2.83295L1.13973 2.28324L2.14855 0.992046L3.24721 1.84347H19.7554L20.854 0.992046L21.8628 2.28324L21.1518 2.83295ZM19.7554 1.84091L11.5 8.23295L3.24464 1.84091L2.14598 0.989489L1.13717 2.28068L1.84565 2.8304L10.6144 9.62131C10.8666 9.81644 11.1768 9.92237 11.4962 9.92237C11.8155 9.92237 12.1257 9.81644 12.3779 9.62131L21.1518 2.83295L21.8603 2.28324L20.8515 0.992046L19.7554 1.84091Z" fill="white"/>
                         </svg>
-                        <a class="ml-4" href="mailto:ftandianus1@gmail.com">ftandianus1@gmail.com</a>
+                        <a class="ml-4" href="mailto:ftandianus@gmail.com">ftandianus@gmail.com</a>
                     </div>
                     <div class="flex items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
