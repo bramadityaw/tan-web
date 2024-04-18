e98fb37d8022db6937cbfa0640d02fe33fe770b3
bramaditya <bramadityaw@duck.com>
2024-04-18 07:01:35 +0700

    added top navigation for admin page

---
+++ resources/views/admin/topnav.blade.php
@@ -0,0 +1,35 @@
+<header>
+    <nav class="flex justify-between px-6 py-4">
+        <div>
+            @include('partials.icons.bars')
+        </div>
+        <div class="">
+            @auth
+            <div class="cursor-pointer" id="profile">
+                <img src="{{ Auth::user()->profile_pic }}" alt="Foto Profil {{ Auth::user()->nama }}" class="h-8 w-8 object-cover rounded-full">
+            </div>
+            @else
+                <a href="/login">Masuk</a>
+            @endauth
+        </div>
+        </nav>
+        @auth
+        <div class="absolute right-8 bg-white rounded-md text-black px-4 py-2 border-2 border-gray-300 hidden">
+            <div class="flex">
+                <div class="">
+                    <p> {{ Auth::user()->name }} </p>
+                    <span class="text-sm text-gray-500">{{ Auth::user()->email }}</span>
+                </div>
+            </div>
+            @include('partials.divider-h')
+            <form action="/logout" method="post">
+                @csrf
+                <button class="flex items-center justify-between w-full" type="submit">
+                    <span>Log Out</span>
+                    @include('partials.icons.logout')
+                </button>
+            </form>
+        </div>
+        @endauth
+    </nav>
+</header>
