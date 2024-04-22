f8192d3a67a07814cce4fc41056469fc7b384f3c
bramaditya <bramadityaw@duck.com>
2024-04-22 21:05:28 +0700

    link to locally hosted fontawesome

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -13,8 +13,9 @@
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
     @vite("resources/css/app.css")
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
+    <link rel="stylesheet" href="/fontawesome/css/fontawesome.min.css"/>
+    <link rel="stylesheet" href="/fontawesome/css/solid.min.css"/>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
     </style>
