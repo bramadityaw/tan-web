a25194c868027f2175c91b8024e5d4b3e9c04985
bramaditya <bramadityaw@duck.com>
2024-04-23 21:29:35 +0700

    configure transactions to be a resource controller

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -9,9 +9,50 @@ use Illuminate\View\View;
 class TransactionsController extends Controller
 {
     public function index() : View {
-        return view('admin.dashboard.transactions', [
+        return view('admin.dashboard.transactions.index', [
             "sales" => DB::table('sales')->paginate(5),
             "purchases" => DB::table('purchases')->paginate(5),
         ]);
+    }
+
+    public function create() : View {
+        return view('admin.dashboard.transactions.create');
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
