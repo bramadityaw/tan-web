e0315d59e8832cf4829dd02a238382672b77aec1
bramaditya <bramadityaw@duck.com>
2024-04-25 12:52:52 +0700

    change to valid validation strings

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Enums\JenisTransaksi;
-use Illuminate\Validation\Rule;
+use App\Models\Purchase;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rules\Enum;
 use Illuminate\View\View;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
@@ -24,36 +25,29 @@ class TransactionsController extends Controller
 
     public function store(Request $request) : RedirectResponse
     {
-        $rule = [
-            'jenis_transaksi' => [Rule::enum(JenisTransaksi::class)],
-            'nama_barang' => 'required|string|max:255',
-            'harga_beli' => 'digits:13',
-            'qty' => 'digits_between:0,99',
-            'created_at' => 'date',
-        ];
+        $request->validate(['jenis_transaksi' => [new Enum(JenisTransaksi::class)]]);
+        $transaction_type = $request->input('jenis_transaksi');
 
-        $validated = $request->validate($rule);
-
-        $transaction_type = $validated['jenis_transaksi'];
-
-        $table_name = '';
         switch ($transaction_type) {
             case 'pembelian':
-                $table_name = 'purchases';
-                break;
+                $rule = [
+                   'nama_barang' => ['required', 'string','max:255'],
+                   'harga_beli' => ['required', 'numeric'],
+                   'qty' => ['required', 'integer'],
+                   'created_at' => ['date'],
+                ];
+                $request->validate($rule);
+                Purchase::create($request->except(['jenis_transaksi']));
+            break;
 
             case 'penjualan':
-                $table_name = 'sales';
+//                $table_name = 'sales';
                 break;
 
             default:
                 # code...
                 break;
         }
-
-        unset($validated["jenis_transaksi"]);
-
-        DB::table($table_name)->insert($validated);
 
         return redirect()->intended('admin/dashboard/transactions');
     }
