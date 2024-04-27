4cb756b4352ac9d2d48015af592c20b893b98819
bramaditya <bramadityaw@duck.com>
2024-04-27 09:59:00 +0700

    fix admin popup after refactor

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -19,25 +19,5 @@ function admin($url = '') {
     </div>
 
 @stack('scripts')
-@prepend('scripts')
-
-    <script>
-        const profilePic = document.querySelector('nav #profile');
-        const profileOpts = document.querySelector('nav + div');
-
-
-        function toggleProfileOpts() {
-            if (profileOpts.classList.contains("hidden")){
-                profileOpts.classList.remove("hidden");
-            }
-            else {
-                profileOpts.classList.add("hidden")
-            }
-        }
-
-        profilePic?.addEventListener("click", toggleProfileOpts);
-    </script>
-@endprepend
-
 </body>
 </html>
