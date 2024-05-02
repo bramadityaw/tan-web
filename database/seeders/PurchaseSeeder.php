a36b3790fd91df024a42ac147ad30434f2340829
bramaditya <bramadityaw@duck.com>
2024-05-02 19:50:53 +0700

    more accurately seed data according to schema

--- database/seeders/PurchaseSeeder.php
+++ database/seeders/PurchaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use DateTime;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -16,8 +17,9 @@ class PurchaseSeeder extends Seeder
     {
         DB::table('purchases')->insert([
             'nama_barang' => Str::random(10),
-            'qty' => mt_rand(100000, 999999),
+            'qty' => mt_rand(1, 99),
             'harga_beli' => mt_rand(100000, 999999),
+            'created_at' => date_create(),
         ]);
     }
 }
