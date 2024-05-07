12b476eae224032f2b120f506a76ec8bc3b0c099
bramaditya <bramadityaw@duck.com>
2024-05-07 19:41:03 +0700

    remove vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,2 @@
 import './bootstrap';
 
-import { createApp } from "vue";
-import CreateTransaction from "./components/CreateTransaction.vue"
-
-createApp()
-    .component("create-transaction", CreateTransaction)
-    .mount("#app");
