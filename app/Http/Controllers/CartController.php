27ba325ca22c4e4f501ab3f5b36fb9e3268bd1f4
bramaditya <bramadityaw@duck.com>
2024-05-15 20:14:59 +0700

    insert and show cart implemented

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cart;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
+
+class CartController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Product $product)
+    {
+        $validated = $request->validate([
+            'qty' => ['integer', 'min:1'],
+        ]);
+
+        if (!DB::table('carts')->where('product_id', $product->id)->exists())
+        {
+            Cart::create([
+                'user_id' => Auth::user()->id,
+                'product_id' => $product->id,
+                'quantity' => $validated['qty'],
+            ]);
+        }
+
+        return redirect()->to('/cart');
+    }
+
+    public function index() : View
+    {
+        $cart = DB::table('carts')
+            ->where('user_id', '=', Auth::user()->id)
+            ->get();
+
+        $cart_contents = $cart->map(function ($item) {
+            return (object) ["product" => Product::find($item->product_id),
+                             "qty"     => $item->quantity];
+        });
+
+        return view('toko.cart', [
+            'contents' => $cart_contents,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- resources/views/toko/cart.blade.php
+++ resources/views/toko/cart.blade.php
@@ -1,12 +1,40 @@
-@auth
-<a target="_blank" href="/cart/{id}">
-@else
-<a href="/login">
-@endauth
-    <div class="rounded-full px-2 py-1 mr-4 md:mr-0 text-lg text-gray-500 hover:text-gray-800 bg-white hover:bg-gray-300">
-        <div class="pt-1 pr-1">
-             <i class="fa-solid fa-cart-shopping"></i>
-        </div>
-        <span class="absolute ml-[1.25rem] mt-[-.75rem] rounded-full px-1 text-xs text-white bg-orange-600">1</span>
-    </div>
-</a>
+@php
+function rupiah(int $amount) : string {
+    return 'Rp.' . number_format($amount,2, ',' , '.');
+}
+@endphp
+
+@extends('layouts.user')
+
+@section('main')
+<section class="bg-[#1B3C73] py-8">
+    <div class="w-5/6 md:w-full max-w-[520px] mx-auto">
+        <div class="rounded-md bg-white">
+            <div class="px-6 pt-5 pb-1">
+            @foreach ($contents as $content)
+                <div class="flex mb-4">
+                    <a class="block w-full" href="/toko/produk/{{ $content->product->id }}">
+                        <img class="w-full max-w-[192px] aspect-auto" src="/storage/{{ $content->product->thumbnail_url }}" alt="{{ $content->product->nama }}">
+                    </a>
+                    <div class="ml-4 md:m-0 md:flex">
+                        <a class="mr-4 md:mr-8" href="/toko/produk/{{ $content->product->id }}">
+                            <h1>{{ $content->product->nama }}</h1>
+                            <p class="text-sm">{{ rupiah($content->product->harga) }}</p>
+                        </a>
+                        <div class="flex min-w-[135px] w-full text-sm items-center">
+                            <button type="button" onclick="qty.value ++; updateQty(qty.value)" class="h-10 border border-r-0 rounded-l px-3">
+                               <i class="fa-solid fa-plus"></i>
+                            </button>
+                            <input type="number" name="qty" id="qty" class=
+                            "h-10 border mt-1 px-4 w-full bg-gray-50" value="{{ $content->qty }}" min=
+                            "1">
+                            <button type="button" onclick="qty.value > 0 ? qty.value-- : 0; updateQty(qty.value)" class="h-10 border border-l-0 rounded-r px-3">
+                              <i class="fa-solid fa-minus"></i>
+                           </button>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+            </div>
+</section>
+@endsection
