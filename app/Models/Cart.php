adde3fc706f947afe2293908fd9f30fa2713f6ea
bramaditya <bramadityaw@duck.com>
2024-05-15 20:11:54 +0700

    show amount of items in cart

---
+++ app/Models/Cart.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class Cart extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'quantity',
+        'user_id',
+        'product_id'
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'updated_at' => 'datetime',
+    ];
+
+    public static function count()
+    {
+        return DB::table('carts')->where('user_id', '=', Auth::user()->id)
+                                 ->get()->count();
+    }
+}
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,3 +1,5 @@
+@use(App\Models\Cart)
+
 <header class="bg-[#1b3c73] sticky top-0 z-10">
     <nav class=" flex px-6 py-4 text-white items-center justify-between">
         @include('partials.icons.logo')
@@ -14,7 +16,18 @@
             <a href="/toko">Toko</a>
             <a href="/blog">Blog</a>
         </div>
-        @include('toko.cart')
+        @auth
+        <a target="_blank" href="/cart">
+        @else
+        <a href="/login">
+        @endauth
+            <div class="rounded-full px-2 py-1 mr-4 md:mr-0 text-lg text-gray-500 hover:text-gray-800 bg-white hover:bg-gray-300">
+                <div class="pt-1 pr-1">
+                     <i class="fa-solid fa-cart-shopping"></i>
+                </div>
+                <span class="absolute ml-[1.25rem] mt-[-.75rem] rounded-full px-1 text-xs text-white bg-orange-600">{{ Auth::check() ? Cart::count() : 0 }}</span>
+            </div>
+        </a>
         <div class="">
             @auth
             <div class="cursor-pointer" id="profile">
