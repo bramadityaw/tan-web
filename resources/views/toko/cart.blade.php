f26b4a17323bc432d92beaa9bfb5cca9d9368a3e
bramaditya <bramadityaw@duck.com>
2024-05-16 09:30:51 +0700

    adjust layout slightly

--- resources/views/toko/cart.blade.php
+++ resources/views/toko/cart.blade.php
@@ -9,22 +9,28 @@ function rupiah(int $amount) : string {
 @section('main')
 <section class="bg-[#1B3C73] py-8">
     <div class="w-5/6 md:w-full max-w-[600px] mx-auto">
-        <div class="rounded-md bg-white">
+       <div class="rounded-md bg-white">
+            <div class="flex justify-between pt-4 px-6">
+                <a class="flex items-center w-fit text-center text-sm md:text-md" href="/toko">
+                    <i class="fa-solid fa-arrow-left border border-black rounded-full p-1 mr-4 "></i>
+                    <p>Kembali</p>
+                </a>
+            </div>
             <div class="px-6 pt-5 pb-1">
             @foreach ($contents as $content)
-                <div class="flex mb-4">
-                    <button data-cart-id="{{ $content->cart }}" onclick="window.cartItem = this; deleteItem(window.cartItem)" class="border border-gray-600 text-gray-800 rounded-md px-3 py-2 mr-4">
-                        <i class="fa-solid fa-trash"></i>
-                    </button>
-                    <a class="block w-full" href="/toko/produk/{{ $content->product->id }}">
-                        <img class="w-full max-w-[192px] aspect-auto" src="/storage/{{ $content->product->thumbnail_url }}" alt="{{ $content->product->nama }}">
+                <div class="flex items-center mb-4">
+                    <a class="block w-full max-w-[192px]" href="/toko/produk/{{ $content->product->id }}">
+                        <img class="w-full aspect-auto" src="/storage/{{ $content->product->thumbnail_url }}" alt="{{ $content->product->nama }}">
                     </a>
-                    <div class="ml-4 md:m-0 md:flex">
-                        <a class="mr-4 md:mr-8" href="/toko/produk/{{ $content->product->id }}">
+                    <div class="ml-4 md:m-0 md:flex md:w-full md:justify-between">
+                        <a class="mx-4 md:mr-8" href="/toko/produk/{{ $content->product->id }}">
                             <h1>{{ $content->product->nama }}</h1>
-                            <p class="text-sm">{{ rupiah($content->product->harga) }}</p>
+                            <p class="text-sm w-fit">{{ rupiah($content->product->harga) }} / ekor</p>
+                            <button data-cart-id="{{ $content->cart }}" onclick="window.cartItem = this; deleteItem(window.cartItem)" class="border border-gray-600 text-gray-800 rounded-md px-2 py-1 mt-4">
+                                <i class="text-xs fa-solid fa-trash"></i>
+                            </button>
                         </a>
-                        <div data-id="{{ $content->cart }}" class="flex min-w-[135px] w-full text-sm items-center">
+                        <div data-id="{{ $content->cart }}" class="flex min-w-[135px] max-w-[155px] w-full text-sm items-center">
                             <button type="button" data-id="{{ $content->cart }}" onclick="updateTotal('+', this.dataset.id )" class="h-10 border border-r-0 rounded-l px-3">
                                <i class="fa-solid fa-plus"></i>
                             </button>
@@ -38,18 +44,18 @@ function rupiah(int $amount) : string {
                     </div>
                 </div>
             @endforeach
-                <div class="md:flex mb-4">
-                    <div class="flex">
+                <div class="md:flex md:items-center mb-4">
+                    <div class="flex items-center">
                         <p class="w-3/5"> Total Pembayaran*
                             <span class="float-start text-xs">*tidak termasuk ongkos pengiriman</span>
                         </p>
                         <p id="total" class="mx-4">{{ rupiah($total) }}</p>
                     </div>
                     <div data-total="{{ $total }}" hidden></div>
-                    <form action="" method="post" class="w-1/3">
+                    <form action="" method="post" class="md:w-1/3">
                         @csrf
                         <input type="hidden">
-                        <button class="flex items-center bg-[#1B3C73] rounded-md w-full font-semibold text-white text-center text-sm md:text-md px-2 py-1" type="submit">
+                        <button class="flex items-center bg-[#1B3C73] rounded-md w-full font-semibold text-white text-center text-sm md:text-md mt-4 md:mt-0 px-2 py-1" type="submit">
                             <i class="fa-solid fa-bag-shopping block text-md mr-4"></i>
                             <span class="inline-block mr-2">Checkout</span>
                         </button>
@@ -58,7 +64,7 @@ function rupiah(int $amount) : string {
             </div>
         </div>
 </section>
-<dialog id="deleteDialog" class="border border-[#1B3C73] rounded-md p-4 w-4/5 md:w-1/6">
+<dialog id="deleteDialog" class="border border-[#1B3C73] rounded-md p-4 w-2/5 md:4/5 lg:w-1/6">
     <div class="text-center">
         <button class="float-end" type="button" onclick="deleteDialog.close()">
             <i class="fa-solid fa-times text-lg"></i>
