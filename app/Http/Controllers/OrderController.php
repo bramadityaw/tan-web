1fe787359b00bdade31a684610bf6e88848d6b93
bramaditya <bramadityaw@duck.com>
2024-05-16 21:20:39 +0700

    user can make orders

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Product;
+use Carbon\Carbon;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    public function showVerify(Order $order) : View
+    {
+        return view('order.verify', [
+            "order" => $order,
+        ]);
+    }
+
+    public function verify(Request $request, Order $order) : RedirectResponse
+    {
+        return redirect()->intended('/admin/dashboard');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request) : RedirectResponse
+    {
+        $cart_item_ids = collect($request->except('_token'))->values();
+        $cart_exists = $cart_item_ids
+            ->map(function ($id) {
+                return DB::table('carts')->where('id', '=', $id)->exists();
+            })
+            ->reduce(function ($acc, $curr) {
+                return $acc && $curr;
+            }, true);
+
+        if (!$cart_exists)
+        {
+            $validator = \Illuminate\Support\Facades\Validator::make([], []);
+
+            $validator->errors()->add('cart_id', 'Cart does not exist');
+
+            throw new \Illuminate\Validation\ValidationException($validator);
+        } else {
+            $cart_items = $cart_item_ids
+            ->map(function ($id) {
+                return DB::table('carts')->where('id', '=', $id)->get();
+            })->collapse();
+
+            $order = Order::create([
+                "expired_date" => Carbon::parse(time() + 24 * 60 * 60)->toDateTimeString(),
+                "harga_total" => CartController::total(),
+                "user_id" => Auth::user()->id,
+            ]);
+
+            foreach ($cart_items as $item)
+            {
+                OrderItem::create([
+                    "price" => Product::findOrFail($item->product_id)->harga,
+                    "quantity" => $item->quantity,
+                    "order_id" => $order->id,
+                    "product_id" => $item->product_id,
+                ]);
+            }
+            return redirect()->intended('/order/' . (string) $order->id . '/verify');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ app/Models/OrderItem.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'price',
+        'quantity',
+        'product_id',
+        'order_id',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'updated_at' => 'datetime',
+    ];
+}
