f63db12e6bd056935978d0f69a495cff3f4c3e01
bramaditya <bramadityaw@duck.com>
2024-05-17 14:03:31 +0700

    make Carbon wrapper util to format date into Indonesian locale

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,5 +1,11 @@
 <?php
 
+use Carbon\Carbon;
+
 function rupiah(int $amount) : string {
     return 'Rp.' . number_format($amount,2, ',' , '.');
+}
+
+function tanggalIdn(string $date, string $format) : string {
+    return Carbon::parse($date)->translatedFormat($format);
 }
