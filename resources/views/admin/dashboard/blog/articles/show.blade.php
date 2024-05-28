749ec781c5a350d336ed19e80852f83406ba0d8d
bramaditya <bramadityaw@duck.com>
2024-05-28 14:18:32 +0700

    fix layout

--- resources/views/admin/dashboard/blog/articles/show.blade.php
+++ resources/views/admin/dashboard/blog/articles/show.blade.php
@@ -8,14 +8,16 @@
 </div>
 <div class="w-5/6 sm:w-3/5 md:w-1/2 mx-auto">
     <div class="bg-white px-5 py-4 rounded-md mb-4">
-        <div class="flex mb-4 items-center">
-            <div class="flex flex-col justify-between w-full">
-                <img class="max-h-[128px]" src="/storage/images/{{ $article->thumbnail_url }}" alt="{{ $article->judul }}">
-                <h1 class="flex justify-between">Judul: <span class="font-bold text-lg">{{ $article->judul }}</span></h1>
-                <p class="flex justify-between">Kategori: <span >{{ \App\Models\Kategori::find($article->kategori_id)->value }}</span></p>
-                <span class="mt-1">Konten:</span>
-                <textarea class="rounded-md text-sm px-3 py-2" cols="30" rows="10" disabled>{{ trim($article->konten) }}</textarea>
+        <div class="w-full">
+            <div class="flex">
+                <div class="mr-4">
+                    <h1 class="flex justify-between">Judul: <span class="ml-5 font-bold text-lg">{{ $article->judul }}</span></h1>
+                    <p class="flex justify-between">Kategori: <span >{{ \App\Models\Kategori::find($article->kategori_id)->value }}</span></p>
+                </div>
+                <img class="max-h-[128px] aspect-auto" src="/storage/images/{{ $article->thumbnail_url }}" alt="{{ $article->judul }}">
             </div>
+            <p class="mt-1">Konten:</p>
+            <textarea class="rounded-md text-sm px-3 py-2 w-full" cols="30" rows="10" disabled>{{ trim($article->konten) }}</textarea>
         </div>
     </div>
 </div>
