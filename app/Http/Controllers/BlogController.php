e7c2ca174a5dbaa52b88b931db1137cedab4008d
bramaditya <bramadityaw@duck.com>
2024-05-28 13:46:59 +0700

    handle blog page requests

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use App\Models\Kategori;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
+
+class BlogController extends Controller
+{
+    public function index() : View
+    {
+        return view('blog.index', [
+            "categories" => DB::table('kategori')
+                ->get()
+                ->map(function ($category) {
+                    $count = Article::where('kategori_id', $category->id)
+                        ->get()
+                        ->count();
+                    return (object) [
+                       "value" => $category->value,
+                       "count" => $count
+                    ];
+                }),
+            "articles" => DB::table('articles')
+                ->paginate(4),
+        ]);
+    }
+
+    public function category(Request $request) : View
+    {
+        $request->validate([
+            'kategori' => 'required|exists:kategori,value',
+        ]);
+
+        [$kategori] = Kategori::where('value', $request->query->get('kategori'))->get();
+
+        return view('blog.index', [
+            "articles" => DB::table('articles')
+                ->where('kategori_id', $kategori->id)
+                ->paginate(4),
+            "categories" => DB::table('kategori')
+                ->get()
+                ->map(function ($category) {
+                    $count = Article::where('kategori_id', $category->id)
+                        ->get()
+                        ->count();
+                    return (object) [
+                       "value" => $category->value,
+                       "count" => $count
+                    ];
+                }),
+        ]);
+    }
+
+    public function search(Request $request) : View
+    {
+        $query_string = $request->query->get('query');
+        return view('blog.index', [
+            "articles" => DB::table('articles')
+                       ->whereFullText('konten', $query_string)
+                       ->paginate(4),
+            "query"    => $query_string,
+        ]);
+    }
+
+    public function show(Article $article) : View
+    {
+        return view('blog.show', [
+            "article" => $article,
+            "others" => DB::table('articles')
+                    ->where('judul', '!=', $article->judul)
+                    ->paginate(4)
+        ]);
+    }
+    //
+}
