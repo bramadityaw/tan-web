7ae4dd3de9b2267d8c0a2817c030e1bc55c674d5
bramaditya <bramadityaw@duck.com>
2024-05-28 14:20:03 +0700

    general blog layout

---
+++ resources/views/layouts/blog.blade.php
@@ -0,0 +1,37 @@
+@include('head')
+@include('navbar')
+
+<div class="px-12 py-10 bg-[#1B3C73] text-white flex">
+@include('blog.asides.left')
+
+<main class="w-full mx-4">
+
+@yield('main')
+
+</main>
+
+@include('blog.asides.right')
+</div>
+
+@include('footer')
+@section('scripts')
+    <script>
+        const token = document.querySelector('input[name="_token"]')?.value;
+        const profilePic = document.querySelector('nav #profile');
+        const profileOpts = document.querySelector('nav + div');
+
+
+        function toggleProfileOpts() {
+            if (profileOpts.classList.contains("hidden")){
+                profileOpts.classList.remove("hidden");
+            }
+            else {
+                profileOpts.classList.add("hidden")
+            }
+        }
+
+        profilePic?.addEventListener("click", toggleProfileOpts);
+    </script>
+@show
+</body>
+</html>
