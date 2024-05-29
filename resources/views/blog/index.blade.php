0fcafd7cffaf194218c029393ed64fad6bf7901b
bramaditya <bramadityaw@duck.com>
2024-05-29 17:12:30 +0700

    add anchors to category and show routes

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -5,13 +5,16 @@
     @foreach($articles as $article)
     <div class="rounded-md bg-white text-black mb-4">
         <article class="px-6 py-5">
-            <h1 class="text-xl font-semibold">{{ ucwords($article->judul) }}</h1>
+            @php $kategori = \App\Models\Kategori::find($article->kategori_id); @endphp
+            <a href="{{ route('blog.show', $article->slug) }}">
+                <h1 class="text-xl font-semibold">{{ ucwords($article->judul) }}</h1>
+            </a>
             <div class="flex gap-4 my-2">
                 <span class="flex gap-2 items-center">
                     <i class="fa-solid fa-clock"></i>
                     <span>{{ tanggalIdn($article->created_at, 'j F, o' ) }}</span>
                 </span>
-                <span>{{ \App\Models\Kategori::find($article->kategori_id)->value }}</span>
+                <a href="/blog/topik{{ $kategori->slug }}">{{ $kategori->value }}</a>
                 @if(! \Carbon\Carbon::parse($article->created_at)
                     ->isSameAs('Y-m-d H:i:s', \Carbon\Carbon::parse($article->updated_at)))
                 <span class="ml-4">Diperbarui {{ tanggalIdn($article->updated_at, 'j F, o') }}</span>
